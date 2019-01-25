bb01ff32f2d50321d9e74ebea19068b8dbaaa46e
Joe Cochran <j_cochran@me.com>
2019-01-24 20:15:36 -0600

    basebutton and fonts

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -1,22 +1,33 @@
 <template>
   <div id="app">
-    <p>{{ message }}</p>
+    <input type="text">
+
+    <BaseButton type="button" text="Submit" />
   </div>
 </template>
 
 <script>
+import { BaseButton } from './components/Base';
+
 export default {
   computed: {
     message() {
       return this.$store.state.message
     }
+  },
+  components: {
+    BaseButton
   }
 }
 </script>
 
-<style scoped>
-p {
-  font-size: 2em;
-  text-align: center;
+<style>
+body {
+  font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif;
+  font-size: 16px;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #0b0c0c;
+  font-weight: 400;
 }
 </style>
---
+++ app/javascript/components/Base/BaseButton.vue
@@ -0,0 +1,59 @@
+<template>
+  <button class="BaseButton">{{ text }}</button>
+</template>
+
+<script>
+export default {
+  name: 'BaseButton',
+  props: {
+    text: String,
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.BaseButton {
+  background-color: #00823b;
+  box-shadow: 0 2px 0 #003618;
+  color: #fff;
+  text-align: center;
+  vertical-align: top;
+  cursor: pointer;
+  appearance: none;
+  border-radius: 3px;
+  border: 2px solid transparent;
+  padding: 7px 10px;
+  margin-top: 0;
+  margin-bottom: 22px;
+  font-size: 16px;
+  display: inline-block;
+  position: relative;
+  box-sizing: border-box;
+  font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif;
+
+  &:hover {
+    background-color: #00692f;
+  }
+
+  &:active {
+    box-shadow: none;
+    top: 2px;
+  }
+
+  &:focus {
+    background-color: #00692f;
+    outline: 3px solid #ffbf47;
+    outline-offset: 0;
+  }
+
+  &:disabled,
+  &:disabled:hover,
+  &:disabled:active {
+    background-color: #00823b;
+    cursor: default;
+    opacity: .5;
+    top: 0;
+    box-shadow: 0 2px 0 #003618;
+  }
+}
+</style>
